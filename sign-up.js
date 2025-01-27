b6ae10ffa5ee02341408f8c5f255a741056e1846
YasinMirnia <ya3mirnia@gmail.com>
2025-01-27 14:05:46 +0330

    add: countdown timer

--- sign-up.js
+++ sign-up.js
@@ -1,9 +1,27 @@
-//Active int-tel-input library
-var input = document.querySelector("#phone");
-var iti = window.intlTelInput(input, {
-  //Defult Country
-  initialCountry: "pk",
-  preferredCountries: ["pk"],
-  utilsScript:
-    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js",
-});
+// the date that counting down
+let countDownDate = new Date("Feb 28, 2025 21:00:00").getTime();
+
+//update in any second;
+let x = setInterval(function () {
+  //Get today date and time
+  let now = new Date().getTime();
+
+  //find the distance between now and countdown time
+  let distance = countDownDate - now;
+
+  //time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  //Disply the result in the element for timeCountDown
+  document.getElementById("countdown-timer").innerHTML =
+    days + " : " + hours + " : " + minutes + " : " + seconds;
+
+  //if the countDown finished, write a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown-timer").innerHTML = "Time for draw";
+  }
+}, 1000);
