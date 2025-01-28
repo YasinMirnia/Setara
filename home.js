8b719188248b58de86bb9e5d20b5d90625ad68c9
YasinMirnia <ya3mirnia@gmail.com>
2025-01-28 15:41:22 +0330

    header designed

--- home.js
+++ home.js
@@ -25,3 +25,14 @@ let x = setInterval(function () {
     document.getElementById("countdown-timer").innerHTML = "Time for draw";
   }
 }, 1000);
+
+document.getElementById("search-icon").addEventListener("click", function () {
+  var searchBar = document.getElementById("search-bar");
+
+  // اگر جعبه جستجو پنهان بود، آن را نمایش می‌دهیم
+  if (searchBar.style.display === "none" || searchBar.style.display === "") {
+    searchBar.style.display = "block";
+  } else {
+    searchBar.style.display = "none"; // پنهان کردن جعبه جستجو
+  }
+});
