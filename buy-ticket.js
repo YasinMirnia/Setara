1b0459509dcfe4560ef17b6f203ed36fbc0cb2ae
YasinMirnia <ya3mirnia@gmail.com>
2025-02-08 03:00:16 +0330

    design: header designed

--- buy-ticket.js
+++ buy-ticket.js
@@ -8,3 +8,31 @@ document.getElementById("search-icon").addEventListener("click", function () {
     searchBar.style.display = "none"; // پنهان کردن جعبه جستجو
   }
 });
+
+// the date that counting down
+let countDownDate = new Date("Feb 28, 2025 21:00:00").getTime();
+
+//update in any second;
+let x = setInterval(function () {
+  //Get today date and time
+  let now = new Date().getTime();
+
+  //find the distance between now and countdown time
+  let distance = countDownDate - now;
+
+  //time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  //Disply the result in the element for timeCountDown
+  document.getElementById("countdown-timer").innerHTML =
+    days + " : " + hours + " : " + minutes + " : " + seconds;
+
+  //if the countDown finished, write a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown-timer").innerHTML = "Time for draw";
+  }
+}, 1000);
