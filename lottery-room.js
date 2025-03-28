3ad667fc3884dce47b7748bd53e2fb49444f6a69
YasinMirnia <ya3mirnia@gmail.com>
2025-03-28 19:17:33 +0330

    search bar has been deleted

--- lottery-room.js
+++ lottery-room.js
@@ -28,11 +28,4 @@ let x = setInterval(function () {
 
 document.getElementById("search-icon").addEventListener("click", function () {
   var searchBar = document.getElementById("search-bar");
-
-  // اگر جعبه جستجو پنهان بود، آن را نمایش می‌دهیم
-  if (searchBar.style.display === "none" || searchBar.style.display === "") {
-    searchBar.style.display = "block";
-  } else {
-    searchBar.style.display = "none"; // پنهان کردن جعبه جستجو
-  }
 });
