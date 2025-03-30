06e220bd594617842a1ae97a0f746d36229d5c8b
YasinMirnia <ya3mirnia@gmail.com>
2025-03-31 00:24:31 +0330

    html css and js of lottery room

--- lottery-room.js
+++ lottery-room.js
@@ -26,6 +26,16 @@ let x = setInterval(function () {
   }
 }, 1000);
 
-document.getElementById("search-icon").addEventListener("click", function () {
-  var searchBar = document.getElementById("search-bar");
-});
+let wheel = document.querySelector(".wheel");
+let inner = document.querySelector(".inner");
+let spinBtn = document.querySelector(".spinBtn");
+
+let value1 = Math.ceil(Math.random() * -3600);
+let value2 = Math.ceil(Math.random() * 3600);
+
+spinBtn.onclick = function () {
+  wheel.style.transform = "rotate(" + value1 + "deg)";
+  inner.style.transform = "rotate(" + value2 + "deg)";
+  value1 += Math.ceil(Math.random() * -3600);
+  value2 += Math.ceil(Math.random() * 3600);
+};
